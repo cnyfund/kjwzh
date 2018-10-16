80966cb330a73ca92c6075b014411a96b6fe914b
Ubuntu <ubuntu@ip-172-31-15-45.ap-northeast-1.compute.internal>
2018-10-16 23:02:11 +0000

    add backup script

--- backup.sh
+++ backup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+echo "`date -u` run settlement: /usr/bin/curl -v -k https://www.9lp.com/settlement.php"
+output=$(/usr/bin/curl -v -k https://www.9lp.com/settlement.php)
+echo "`date -u` settlement output is $output"
+echo "`date -u` settlement return is $?"
+
 POSBACKUPSQL=pos-backup-$(date +%F).sql
-/usr/bin/mysqldump --defaults-extra-file=/home/ubuntu/.my.cnf --single-transaction --quick --lock-tables=false --all-databases > /home/ubuntu/workspace/kjwzh/backup/$POSBACKUPSQL
-/home/ubuntu/.local/bin/aws s3 cp /home/ubuntu/workspace/kjwzh/backup/$POSBACKUPSQL s3://elasticbeanstalk-us-west-2-551441213847/POSBackup/
+echo "echo `date -u` sudo /usr/bin/mysqldump --defaults-extra-file=/home/ubuntu/.my.cnf --single-transaction --quick --lock-tables=false --all-databases > /home/ubuntu/workspace/kjwzh/backup/$POSBACKUPSQL"
+output=$(sudo /usr/bin/mysqldump --defaults-extra-file=/home/ubuntu/.my.cnf --single-transaction --quick --lock-tables=false --all-databases > /home/ubuntu/workspace/kjwzh/backup/$POSBACKUPSQL)
+echo "`date -u` backup return $output $?"
+
+echo "`date -u` sudo /home/ubuntu/.local/bin/aws s3 cp /home/ubuntu/workspace/kjwzh/backup/$POSBACKUPSQL s3://elasticbeanstalk-us-west-2-551441213847/POSBackup/"
+output=$(/home/ubuntu/.local/bin/aws s3 cp /home/ubuntu/workspace/kjwzh/backup/$POSBACKUPSQL s3://elasticbeanstalk-us-west-2-551441213847/POSBackup/)
+
+echo "`date -u` upload return $output $?"
