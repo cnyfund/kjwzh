d40240a51da165bed6b7ff6833c7b8190af0329c
Tao Zhang <taozhang@salesforce.com>
2019-08-18 17:15:41 -0700

    adding missing files

---
+++ error/proxyerror.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+<script type="text/javascript" src="/ui/js/jquery.min.js"></script>
+<script type="text/javascript" src="/ui/js/bootstrap.min.js"></script>
+
+<title>403</title>
+<style>
+	h3{
+		font-size:60px;
+		color:#0;
+		text-align:center;
+		padding-top:30px;
+		font-weight:normal;
+	}
+</style>
+</head>
+
+<body>
+<?php $error_code = isset($_REQUEST['error_code'])? $_REQUEST['error_code']:"403";
+      $error_msg =  isset($_REQUEST['error_msg'])? $_REQUEST['error_msg']: "服务器上文件或目录拒绝访问!";
+      $return_url = isset($_REQUEST['return_url'])? $_REQUEST['return_url']: "";
+?>
+<div class="container">
+<div class="row">
+<h3><?php echo $error_code . ", " . $error_msg; ?></h3>
+</div>
+<?php if (!empty($return_url)):?>
+<div class="row">
+<div class="col-md-*-auto">
+<p class="text-center"><button type="button" id="btn-back" class="btn btn-big btn-primary">返回</button></p>
+</div>
+</div>
+<?php endif ?>
+</div>
+<?php if (!empty($return_url)):?>
+<script>
+    $(document).ready(function(){
+        $("#btn-back").click(function(){
+           window.location.href = "<?php echo $return_url; ?>";     
+        });
+    });
+</script>
+<?php endif ?>
+</body>
+</html>
